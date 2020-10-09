e38f8e345e1d2d935c1505f28a759b64be4c0040
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-10-09 14:33:31 -0500

    added android project named PicstudioLab

---
+++ PicStudioLab/app/src/main/java/com/example/picstudiolab/Frames.java
@@ -0,0 +1,14 @@
+package com.example.picstudiolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frames);
+    }
+}
