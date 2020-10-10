572dd116c475b202088176ba00117acf51edf874
bharathkumar3355 <bharathkumar3355@gmail.com>
2020-10-09 22:12:25 -0500

    new java files

---
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ConfirmationPage.java
@@ -0,0 +1,14 @@
+package com.example.picstudiolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_page);
+    }
+}
