89ea711abb19f17b753b712b6fbbbc28a9f13429
bharathkumar3355 <bharathkumar3355@gmail.com>
2020-10-30 15:02:59 -0500

    adding map functionality

--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/Map.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/Map.java
@@ -1,5 +1,6 @@
 package com.example.picstudiolab;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -17,26 +19,16 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_main);
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.google_map);
         supportMapFragment.getMapAsync(this);
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        gMap=googleMap;
-        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-
-            @Override
-            public void onMapClick(LatLng latLng) {
-                MarkerOptions markerOptions=new MarkerOptions();
-                markerOptions.position(latLng);
-                markerOptions.title(latLng.latitude+ ":" + latLng.longitude);
-                gMap.clear();
-                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                gMap.addMarker(markerOptions);
-            }
-        });
+        LatLng PicStudioLab = new LatLng(40.852, -94.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(PicStudioLab)
+                .title("PicStudioLab"));
     }
 }
