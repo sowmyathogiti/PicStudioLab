650df7f0bcd0d40293b6bef49952a052c228a2af
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-10-30 02:46:26 -0500

    added functionality for FloatingActionButtons in regular FrameSetup.java

--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/Account.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/Account.java
@@ -22,16 +22,44 @@ public class Account extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 EditText firstnamevalue = findViewById(R.id.firstnamevalue);
+
                 if(firstnamevalue.getText().toString().length()==0)
                     firstnamevalue.setError( "First name is required!" );
                 else
                 createAccount();
+
+                EditText lastnamevalue = findViewById(R.id.lastnamevalue);
+                if(lastnamevalue.getText().toString().length()==0)
+                    lastnamevalue.setError( "Last name is required!" );
+                else
+                    createAccount();
+
+                EditText emailvalue = findViewById(R.id.emailvalue);
+                if(emailvalue.getText().toString().length()==0)
+                    emailvalue.setError( "Email Id is required!" );
+                else
+                    createAccount();
+
+                EditText phonevalue = findViewById(R.id.phonevalue);
+                if(phonevalue.getText().toString().length()==0)
+                    phonevalue.setError( "Phone number is required!" );
+                else
+                    createAccount();
+
+                EditText pwdvalue1 = findViewById(R.id.pwdvalue1);
+                EditText pwdvalue2 = findViewById(R.id.pwdvalue2);
+
+                if(!pwdvalue1.getText().toString().equals(pwdvalue2.getText().toString())){
+                    pwdvalue1.setError( "Passwords in two fields should match!" );
+                    pwdvalue2.setError( "Passwords in two fields should match!" );}
+                else
+                    createAccount();
             }
         });
     }
 
     private void createAccount() {
-        Intent int2 = new Intent(this,MainActivity.class);
+        Intent int2 = new Intent(this,Account.class);
         startActivity(int2);
     }
 }
--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/FrameSetup.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/FrameSetup.java
@@ -6,20 +6,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FrameSetup extends AppCompatActivity {
 
+    int count1,count2,count3,count9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_setup);
 
         Button cartbtn = findViewById(R.id.cartbtn);
+        FloatingActionButton add1 = findViewById(R.id.add1);
+        FloatingActionButton add2 = findViewById(R.id.add2);
+        FloatingActionButton add3 = findViewById(R.id.add3);
+        FloatingActionButton add9 = findViewById(R.id.add9);
+        final TextView addvalue1 = (TextView) findViewById(R.id.addvalue1);
+        final TextView addvalue2 = findViewById(R.id.addvalue2);
+        final TextView addvalue3 = findViewById(R.id.addvalue3);
+        final TextView addvalue9 = findViewById(R.id.addvalue9);
 
         cartbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                cartClick();
+            }
+        });
+
+        add1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count1++;
+                String num = (String.valueOf(count1));
+                addvalue1.setText(num);
+            }
+        });
+
+        add2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count2++;
+                String num = (String.valueOf(count2));
+                addvalue2.setText(num);
+            }
+        });
+
+        add3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count3++;
+                String num = (String.valueOf(count3));
+                addvalue3.setText(num);
+            }
+        });
+
+        add9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count9++;
+                String num = (String.valueOf(count9));
+                addvalue9.setText(num);
             }
         });
     }
