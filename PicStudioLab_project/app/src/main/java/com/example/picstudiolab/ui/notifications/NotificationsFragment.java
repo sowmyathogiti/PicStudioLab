2052042738c13821efc220170ecb196bf96e5ffa
vigneshwar6666 <vigneshwar6666@gmail.com>
2020-11-01 18:15:48 -0600

    added functionality for profile layout page

--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/notifications/NotificationsFragment.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,12 @@
 package com.example.picstudiolab.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +15,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.picstudiolab.MainActivity;
 import com.example.picstudiolab.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NotificationsFragment extends Fragment {
 
@@ -20,9 +25,20 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        ImageView logout = root.findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getContext(), MainActivity.class));
+            }
+        });
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
 //        final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
