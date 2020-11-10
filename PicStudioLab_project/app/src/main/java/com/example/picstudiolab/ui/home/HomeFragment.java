2819fdfa4dfb04a231cd0074481039d0fb12740c
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-11-10 14:46:52 -0600

    new changes related functinality of design in place order

--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/home/HomeFragment.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/home/HomeFragment.java
@@ -28,15 +28,15 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        Button locationbtn = root.findViewById(R.id.location);
+//        Button locationbtn = root.findViewById(R.id.location);
 
-        locationbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cartInt = new Intent(getActivity(), Map.class);
-                startActivity(cartInt);
-            }
-        });
+//        locationbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent cartInt = new Intent(getActivity(), Map.class);
+//                startActivity(cartInt);
+//            }
+//        });
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
