73e89c1f9ac8069911776601f6585025bb0c044f
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-11-22 19:10:54 -0600

    modified home activity functionalities.

--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ZoomInvitation.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ZoomInvitation.java
@@ -2,13 +2,29 @@ package com.example.picstudiolab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ZoomInvitation extends AppCompatActivity {
-
+    TextView link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoom_invitation);
+
+        link = findViewById(R.id.link);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri zoomlink = Uri.parse("https://zoom.us/j/98769858501?pwd=K0NKQm5uUUpiYWVGaFlxMm5LcEZ2QT09");
+                Intent zoomIntent = new Intent(Intent.ACTION_VIEW, zoomlink);
+                startActivity(zoomIntent);
+
+            }
+        });
     }
 }
--- PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/home/HomeFragment.java
+++ PicStudioLab_project/app/src/main/java/com/example/picstudiolab/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.picstudiolab.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +19,27 @@ import com.example.picstudiolab.BrowsePics;
 import com.example.picstudiolab.Map;
 import com.example.picstudiolab.R;
 import com.example.picstudiolab.Wedding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
+    FirebaseAuth mAuth1; //= FirebaseAuth.getInstance();
+    FirebaseFirestore db1;
+    private HomeViewModel homeViewModel;
+    TextView initialmsg;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        mAuth1 = FirebaseAuth.getInstance();
+        db1 = FirebaseFirestore.getInstance();
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -37,11 +52,32 @@ public class HomeFragment extends Fragment {
 //                startActivity(cartInt);
 //            }
 //        });
-
+        initialmsg = root.findViewById(R.id.initialmsg);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 
+            }
+        });
+        DocumentReference documentReference = db1.collection("customers").document(mAuth1.getCurrentUser().getUid());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        Log.d("exist", "DocumentSnapshot data: " + documentSnapshot.getData());
+
+                        initialmsg.setText("Hello "+(CharSequence) documentSnapshot.get("Name")+" welcome home");
+                    }
+                    else{
+                        Log.d("not exist", "DocumentSnapshot data: " + documentSnapshot.getData());
+                    }
+                }
+                else{
+                    Log.d("data", "DocumentSnapshot data: " + task.getException());
+                }
             }
         });
         return root;
